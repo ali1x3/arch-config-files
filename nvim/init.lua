cda0094f0644ad8c588d826497ca5d36a9d00204
ali1x3 <avilaalex371@gmail.com>
2025-02-15 03:30:19 +0000

    please work this time

---
+++ nvim/init.lua
@@ -0,0 +1,10 @@
+require("niguel")
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=4")
+vim.cmd("set softtabstop=4")
+vim.cmd("set shiftwidth=4")
+
+local opt = vim.opt
+
+opt.relativenumber = true
+opt.number = true
