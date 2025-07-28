90451eb0edd1f741b91d0cbd0e6f9eb937931198
ali1x3 <avilaalex371@gmail.com>
2025-07-28 20:59:35 +0800

    updated the configs, not sure what changed

--- nvim/init.lua
+++ nvim/init.lua
@@ -5,9 +5,13 @@ vim.cmd("set tabstop=4")
 vim.cmd("set softtabstop=4")
 vim.cmd("set shiftwidth=4")
 vim.cmd("set scrolloff=8")
+vim.cmd("let g:db_ui_auto_execute_table_helpers = 1")
+vim.cmd("set nofoldenable")
+vim.diagnostic.config({ virtual_text = true })
 vim.cmd("source ~/.vimrc")
+
 
 local opt = vim.opt
 opt.relativenumber = true
 opt.number = true
-
+opt.conceallevel = 1
--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -4,6 +4,12 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("v", "J", ":m '>+1<cr>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<cr>gv=gv")
 
+-- Move between windows using Ctrl + h/j/k/l
+vim.keymap.set('n', '<C-h>', '<C-w>h')
+vim.keymap.set('n', '<C-j>', '<C-w>j')
+vim.keymap.set('n', '<C-k>', '<C-w>k')
+vim.keymap.set('n', '<C-l>', '<C-w>l')
+
 
 vim.keymap.set("x", "<leader>p", "\"_dP")
 
@@ -12,5 +18,4 @@ vim.keymap.set("v", "<leader>y", "\"+y")
 vim.keymap.set("n", "<leader>y", "\"+y")
 
 vim.keymap.set("v", "A", ":norm A")
-
 vim.keymap.set("n", "<leader>s", ":%s/\\<<C-r><C-w>\\>/<C-r><C-w>/g<Left><Left>")
