90a108655f896cedca2691dd1dedeb3fd0c2a1e3
ali1x3 <avilaalex371@gmail.com>
2025-11-11 14:19:55 +0800

    added working keyboard toggle to laptop config, also new wallpaper

---
+++ waybar_laptop/.config/waybar/toggle_laptop_keyboard.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+
+#~/.config/waybar/toggle-laptop-keyboard.sh &
+#  _____                 _             
+# |_   _|__   __ _  __ _| | ___  
+#   | |/ _ \ / _` |/ _` | |/ _ \ 
+#   | | (_) | (_| | (_| | |  __/    
+#   |_|\___/ \__, |\__, |_|\___|    
+#            |___/ |___/                          
+#
+
+export XDG_RUNTIME_DIR="/run/user/$(id -u)"
+SOCKET_PATH=$(find "$XDG_RUNTIME_DIR/hypr" -type s -name "*.socket.sock" 2>/dev/null | head -n1)
+export HYPRLAND_INSTANCE_SIGNATURE=$(basename "$(dirname "$SOCKET_PATH")")
+CACHE_FILE="/home/niguel/.cache/laptop-keyboard-disabled"
+
+hyprctl -- keyword 'device[evremap-virtual-input-for-/dev/input/event3]:enabled' 0
+
+if [ -f "$CACHE_FILE" ] ;then
+    rm "$CACHE_FILE"
+    hyprctl -r -- keyword 'device[at-translated-set-2-keyboard]:enabled' 1
+    notify-send -u normal "Laptop keyboard enabled"
+else
+    touch "$CACHE_FILE"
+    hyprctl -r -- keyword 'device[at-translated-set-2-keyboard]:enabled' 0
+    notify-send -u normal "Laptop keyboard disabled"
+fi
+
