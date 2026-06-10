4f64028e423dca519f6cc73e33d057dca43ddec6
ali1x3 <avilaalex371@gmail.com>
2026-06-10 10:19:06 +0800

    updated hyprlaptop

--- hypr_laptop/.config/hypr/select_wallpaper.sh
+++ hypr_laptop/.config/hypr/select_wallpaper.sh
@@ -1,27 +1,15 @@
-
 WALLPAPER_DIR="$HOME/wallpapers"
 
-SELECTED_WALLPAPER=$(ls "$WALLPAPER_DIR" | wofi -dmenu "∂" )
+SELECTED_WALLPAPER=$(ls "$WALLPAPER_DIR" | rofi -dmenu "∂" )
  
 if [ -n "$SELECTED_WALLPAPER" ]; then
 
-   # hyprctl hyprpaper preload "$WALLPAPER_DIR/$SELECTED_WALLPAPER"
-   # hyprctl hyprpaper wallpaper ", $WALLPAPER_DIR/$SELECTED_WALLPAPER"
- 
-   SYMLINK_CONFIG_FILE="$HOME/.config/hypr/hyprpaper.conf"
-   SYMLINK_LOCK_CONFIG="$HOME/.config/hypr/hyprlock.conf"
-   TARGET_FILE=$(readlink -f "$SYMLINK_CONFIG_FILE")
-   TARGET_FILE2=$(readlink -f "$SYMLINK_LOCK_CONFIG")
-
    wal -i "$HOME/wallpapers/$SELECTED_WALLPAPER"
+   awww img "$HOME/wallpapers/$SELECTED_WALLPAPER"
 
-   sed -i -e "s|preload = .*|preload = $HOME/wallpapers/$SELECTED_WALLPAPER|" \
-          -e "s|wallpaper = ,.*|wallpaper = ,$HOME/wallpapers/$SELECTED_WALLPAPER|" "$TARGET_FILE"
-   sed -i -e "s|path = .*|path = $HOME/wallpapers/$SELECTED_WALLPAPER|" "$TARGET_FILE2"
+   killall waybar
+   waybar
 
-    pkill hyprpaper
-    hyprpaper
-
-    pkill waybar
-    waybar
+   killall swaync
+   swaync
 fi
